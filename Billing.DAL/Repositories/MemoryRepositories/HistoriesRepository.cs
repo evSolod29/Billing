b426c627f7154391da4863ce4aa09f34de5e8b14
Eugeniy Solodkiy <evsolod29@gmail.com>
2022-11-16 21:56:53 +0300

    Добавлен метод

--- Billing.DAL/Repositories/MemoryRepositories/HistoriesRepository.cs
+++ Billing.DAL/Repositories/MemoryRepositories/HistoriesRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Billing.DAL.Contexts;
 using Billing.DAL.Models;
 
@@ -35,18 +31,34 @@ public class HistoriesRepository
     {
         History? oldHistory = context.Histories.FirstOrDefault(x => x.Id == history.Id);
         if(oldHistory == null)
-            throw new KeyNotFoundException($"History key is {history.Id}");
+            throw new KeyNotFoundException($"History key isn't found. Key is {history.Id}");
 
         User? user = context.Users.FirstOrDefault(x => x.Id == history.User.Id);
         if(user == null)
-            throw new KeyNotFoundException($"User key is {history.User.Id}");
+            throw new KeyNotFoundException($"User key isn't found. Key is {history.User.Id}");
         
         Coin? coin = context.Coins.FirstOrDefault(x => x.Id == history.Coin.Id);
         if(coin == null)
-            throw new KeyNotFoundException($"Coin key is {history.Coin.Id}");
+            throw new KeyNotFoundException($"Coin key isn't found. Key is {history.Coin.Id}");
         
         History newHistory = new History(user, coin) { Id = history.Id };
         context.Histories.Remove(oldHistory);
         context.Histories.Add(newHistory);
+    }
+
+    public async Task<History> Get(long id)
+    {
+        History? history = context.Histories.FirstOrDefault(x => x.Id == id);
+        if(history == null)
+            throw new KeyNotFoundException($"History key isn't found. Key is {id}");
+
+        User newUser = new User(history.User.Name, history.User.Rating) { Id = history.User.Id };
+        User coinUser = new User(history.Coin.User.Name, history.Coin.User.Rating) 
+        {
+            Id = history.Coin.User.Id
+        };
+        Coin newCoin = new Coin(coinUser){ Id = history.Coin.Id };
+
+        return new History(newUser, newCoin) { Id = id };
     }
 }
